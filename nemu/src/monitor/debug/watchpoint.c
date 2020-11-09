b70bc64de7029d2cdac5d9a9874229a064ac2b67
U201713732-Yu Jingyi <jyyu@hust.edu.cn>
2020-11-09 05:52:42 -0800

    temp

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -6,6 +6,9 @@
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
+WP* new_wp();
+void free_wp(WP *wp);
+
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
@@ -24,3 +27,38 @@ void watchpoints_display(){
 	
 }
 
+WP* new_wp(){
+	if(free == NULL){
+		assert(0);
+	}
+	WP* newwp = free_;
+	free_ = free_->next;
+	newwp->next = head;
+	head = newwp;
+	return newwp;
+}
+
+void free_wp(WP *wp){
+	if(wp == NULL || head == NULL){
+		return;
+	}
+	if(wp == head){
+		WP* temp = head;
+		head = head->next;
+		temp->next = free_;
+		free_ = temp;
+		return;
+	}
+	
+	WP *front=head, *temp=head->next;
+	while(temp!=NULL){
+		if(wp == temp){
+			front->next = temp->next;
+			temp->next = free_;
+			free_ = temp;
+			break;
+		}
+		front = temp;
+		temp = temp->next;
+	}
+}
