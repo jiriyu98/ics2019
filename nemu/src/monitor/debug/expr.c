ee2fc8491c8dbfd3f9c6220e94ba2ec0c6276847
tracer-ics2019 <tracer@njuics.org>
2020-11-09 01:15:46 -0800

    >  compile U201713732 root Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:15:46 up 1 day, 11:25,  1 user,  load average: 0.44, 0.51, 0.52 dc787ebac54b9c13f118dd4d1254bc5629adcbc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -336,7 +336,7 @@ uint32_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
-	if(success)
+	if(*success)
 		printf("true");
 	// *success = true;
   return eval(0, nr_token-1, success);
