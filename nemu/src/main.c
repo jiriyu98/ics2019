d4121c1d26a5d91a8184ea258e41c252728f2d1f
tracer-ics2019 <tracer@njuics.org>
2020-11-08 22:14:15 -0800

    >  compile U201713732 root Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:14:15 up 1 day,  8:23,  1 user,  load average: 0.28, 0.40, 0.42 7021f8e1bec88cdc8932758414e1d59c9e1e093

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,7 +11,7 @@ uint32_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  // int is_batch_mode = init_monitor(argc, argv);
+  int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
   // ui_mainloop(is_batch_mode);
