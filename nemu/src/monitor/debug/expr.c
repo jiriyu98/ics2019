65ca63f9f65daa96d84503b6e83d609e62f7d312
tracer-ics2019 <tracer@njuics.org>
2020-11-09 22:13:54 -0800

    >  compile U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:13:54 up 2 days, 18 min,  1 user,  load average: 0.23, 0.32, 0.32 686dc74244d9ed62399e8ca2bd94aa273175a498

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -231,17 +231,6 @@ uint32_t calc(int pos, bool* success){
 int check_parentheses(int p, int q){
   int layer = 0;
   if(tokens[p].type=='(' && tokens[q].type==')'){
-    for(int i=p+1; i<=q-1; i++){
-      if(layer<0){
-        return 0;
-      }
-      if(tokens[i].type == '('){
-				layer++;
-			}
-			if(tokens[i].type == ')'){
-				layer--;
-			}
-    }
 		return 1;
   }
 	for(int i=p; i<=q; i++){
@@ -255,7 +244,10 @@ int check_parentheses(int p, int q){
 			layer--;
 		}
   }
-	return -1;
+	if(layer == 0){
+		return -1;
+	}
+	return 0;
 }
 
 uint32_t eval(int p,int q, bool* success){
