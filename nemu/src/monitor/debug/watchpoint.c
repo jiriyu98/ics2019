7fe72a4392df70aa3d58294a6e4810fc9de8921e
tracer-ics2019 <tracer@njuics.org>
2020-11-10 04:31:26 -0800

    >  compile U201713732 root Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:31:26 up 2 days,  6:35,  1 user,  load average: 0.72, 0.55, 0.49 a435e3c692221e832f0659179c3fffd7b3120f39

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -51,10 +51,10 @@ WP* new_wp(char *exp){
 }
 
 void free_wp(int NO){
-	printf("%d\n", NO);
 	if(head == NULL || NO < 0 || NO >31){
 		return;
 	}
+		printf("%d\n", NO);
 	if(NO == head->NO){
 		WP* temp = head;
 		head = head->next;
