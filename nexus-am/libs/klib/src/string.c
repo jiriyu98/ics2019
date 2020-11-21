ae7d5cca40d56c131c18c283d237ad2a43bab8bc
tracer-ics2019 <tracer@njuics.org>
2020-11-21 01:35:33 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:35:33 up 6 days,  1:57,  1 user,  load average: 3.08, 2.65, 2.63 8203d287693c6bd3e36ff4a753b302fecc39b953

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -43,6 +43,7 @@ int strcmp(const char* s1, const char* s2) {
 		s1++;
 		s2++;
 	}
+		printf("%d\n", *s1 - *s2);
 	return *s1 - *s2;
 }
 
@@ -51,7 +52,6 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 		s1++;
 		s2++;
 	}
-	printf("%d\n", *s1 - *s2);
 	return *s1 - *s2;
 }
 
