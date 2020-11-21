39338b7a3150b07e81cc23fe38d23e648389f2e4
tracer-ics2019 <tracer@njuics.org>
2020-11-21 05:25:20 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:25:20 up 6 days,  4:23,  1 user,  load average: 2.81, 2.77, 2.61 6ab55dea52a5e73891fc8e3d6bd6367281f9a4c0

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -1,22 +1,65 @@
 #include "klib.h"
-#include <stdarg.h>
+#include <stdlib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+	int i;
+	char buf[256];
+	va_list arg = (va_list)((char*)(&fmt)+4);
+	i = vsprintf(buf, fmt, arg);
+	write(buf, i);
+  return i;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+	char* p;
+	char tmp[256];
+	va_list p_next_arg = ap;
+	for (p = out; *fmt; fmt++){
+	  if (*fmt != '%'){
+	      *p++ = *fmt;
+	      continue;
+	  }
+	  fmt++;
+	  switch (*fmt){
+			case 'd':
+				itoa(tmp, *((int*)p_next_arg), 10);
+        strcpy(p, tmp);
+        p_next_arg += 4;
+        p += strlen(tmp);
+        break;
+      case 'x':
+        itoa(tmp, *((int*)p_next_arg), 16);
+        strcpy(p, tmp);
+        p_next_arg += 4;
+        p += strlen(tmp);
+        break;
+      case 's':
+        strcpy(p, tmp);
+        p_next_arg += 4;
+        p += strlen(tmp);
+        break;
+      default:
+        break;
+	  }
+	}
+	return (p - out);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+	va_list ap;
+	int ret;
+	
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+
+	return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+	return 0;
 }
 
 #endif
