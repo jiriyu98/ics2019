172dda180076678e4f0794d944448b6c869c2045
tracer-ics2019 <tracer@njuics.org>
2020-11-21 06:36:53 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:36:53 up 6 days,  5:35,  1 user,  load average: 3.58, 2.98, 2.76 5151b96a565ad8fcba23c7ae14021c2a945eb34

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -6,6 +6,7 @@ int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
+	printf("%s", buf);
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
