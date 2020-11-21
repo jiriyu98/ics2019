2382c946b46db7c5bc4b47a266996c44df0bb1e9
tracer-ics2019 <tracer@njuics.org>
2020-11-21 02:13:23 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:13:23 up 6 days,  2:35,  1 user,  load average: 2.75, 2.67, 2.64 db2f9ada5a101b382df86f331017ce5aea6eab4d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -70,12 +70,12 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-	if(!n) return 0;
-	while(--n && *(char *)s1 == *(char *)s2){
-		(char *)s1++;
-		(char *)s2++;
-	}
-	return *(char *)s1 - *(char *)s2;
+  if(!n) return 0;
+  while(--n && *(char *)s1 == *(char *)s2) {
+	s1 = (char *)s1 + 1;
+	s2 = (char *)s2 + 1;
+  }
+  return (*((unsigned char *)s1) - *((unsigned char *)s2));
 }
 
 #endif
