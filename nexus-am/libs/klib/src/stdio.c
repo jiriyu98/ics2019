99f52b35647c89387abd59cbd2fe637454f95942
tracer-ics2019 <tracer@njuics.org>
2020-11-29 08:12:25 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:12:25 up 6 days, 10:59,  1 user,  load average: 2.42, 2.47, 2.47 b0bc27ac9952a50f22214d91adf88255d07df38a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,131 +5,151 @@
 
 /* refer: https://github.com/likmin/ics2019/blob/3bc63f31e248bfbb87fa4c491b5f146ecf798f76/nexus-am/libs/klib/src/stdio.c */
 
-struct param {
-	char uc:1;
-	char sign;			/* '-' 				 */
-	unsigned int base;	/* number base, e.g 8, 10, 16*/
-	char *bf;			/* buffer for output */
-};
-typedef void (*putcf) (void *, char);
-
-struct _vsprintf_putcf_data {
-	char *dest;
-	size_t num_chars;
-};
-
-static void _vsprintf_putcf(void *p, char c) {
-	struct _vsprintf_putcf_data *data = (struct _vsprintf_putcf_data *)p;
-	data->dest[data->num_chars ++] = c;
+int printf(const char *fmt, ...) {
+  va_list ap;
+  char out[2048];
+  va_start(ap, fmt);
+  vsprintf(out, fmt, ap);
+  va_end(ap);
+  char *ptr = out;
+  while (*ptr) {
+    _putc(*ptr);
+    ptr++;
+  }
+  return 0;
 }
-
-static void _printf_putc(void *p, char c) {  
-	_putc(c);
-}
-
-static void putchw(void *putp, putcf putf, struct param *p) {
-	char ch;
-	char *bf = p->bf;
-	while((ch = *bf++)) 
-		putf(putp, ch);
-}
-
-static void ui2a(unsigned int num, struct param *p) {
-	int n = 0;
-	unsigned int d = 1;
-	char *bf = p->bf;
-	while (num / d >= p->base)
-		d = d * p->base;
-
-	while (d != 0) {
-		int dgt = num /d;
-		num = num % d;
-		d = d / p->base;
-		if (n || dgt > 0 || d == 0) {
-			*bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
-			++n;
-		}
-	}
-	*bf = 0;
-}
-static void i2a (int num, struct param *p) {
-	//printf("num = %d\n", num);
-	if (num < 0) {
-		num = -num;
-		p->sign = '-';
-	}
-	ui2a(num, p);
-}
-
-
-void my_format(void *putp, putcf putf, const char *fmt, va_list va) {
-	struct param p;
-	char bf[12];	/* int = 32b */
-	char ch;
-	p.bf = bf;
-
-	while ((ch = *(fmt++))) {
-		if (ch != '%') {
-			putf(putp, ch);
-		} else {
-			ch = *(fmt++);
-			switch (ch) {
-				case 'd':
-					p.base = 10;
-					p.uc = 0;
-					i2a(va_arg(va, int), &p);
-					putchw(putp, putf, &p);
-					break;
-				case 'x' :
-					p.base = 16;
-					p.uc = 0;
-					i2a(va_arg(va, int), &p);
-					putchw(putp, putf, &p);
-					break;
-				case 's':
-					p.bf = va_arg(va, char *);
-					putchw(putp, putf, &p);
-				    p.bf = bf; //???
-					break;
-				default : break;
-			}
-		}
-
-	}
-}
-
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-	struct _vsprintf_putcf_data data;
-	data.dest = out;
-	data.num_chars = 0;
-	my_format(&data, _vsprintf_putcf, fmt, ap);
-	data.dest[data.num_chars] = '\0';
+  int t;
+  int sign = 0;
+  unsigned int lim = 0;
+  char *ptr = NULL;
+  char *start = out;
+  char buffer[50];
+  char Representation[] = "0123456789abcdef";
 
-	return data.num_chars;
+  while (*fmt) {
+    /*
+       if (*fmt == '\') {
+       switch (*++fmt) {
+       case 'n':
+     *start++ = '\n';
+     default:
+     assert(0);
+     }
+     }
+     */
+
+    if (*fmt == '%') {
+      if (*(fmt + 1) == '0') {
+        fmt++;
+        lim = 0;
+        for (int k = 0; k <= 9; k++) {
+          if (*(fmt + 1) == Representation[k]) {
+            lim = k;
+            fmt++;
+            break;
+          }
+        }
+      }
+
+      switch (*++fmt) {
+        case 'd':
+          t = va_arg(ap, int);
+          ptr = &buffer[49];
+          *ptr = '\0';
+          if (t < 0) {
+            if (t == (-2147483647 - 1)) {
+              printf("INT_MIN!!!\n");
+              assert(0);
+            }
+            t = -t;
+            sign = 1;
+          }
+          do {
+            *--ptr = Representation[t % 10];
+            t /= 10;
+          } while (t);
+          while (&buffer[49] - ptr < lim) {
+            *--ptr = Representation[0];
+          }
+          if (sign) *--ptr = '-';
+          while (*ptr) {
+            *start = *ptr;
+            start++;
+            ptr++;
+          }
+          break;
+
+        case 'x':
+          t = va_arg(ap, unsigned int);
+          ptr = &buffer[49];
+          *ptr = '\0';
+          do {
+            *--ptr = Representation[t % 16];
+            t /= 16;
+          } while (t);
+          while (&buffer[49] - ptr < lim) {
+            *--ptr = Representation[0];
+          }
+          while (*ptr) {
+            *start++ = *ptr++;
+          }
+          break;
+
+        case 'c':
+          *start++ = (char)va_arg(ap, int);
+          break;
+
+        case 's':
+          ptr = va_arg(ap, char *);
+          while (*ptr) {
+            *start++ = *ptr++;
+          }
+          break;
+
+        case 'p':
+          t = va_arg(ap, unsigned int);
+          ptr = &buffer[49];
+          *ptr = '\0';
+          do {
+            *--ptr = Representation[t % 16];
+            t /= 16;
+          } while (t);
+          while (&buffer[49] - ptr < lim) {
+            *--ptr = Representation[0];
+          }
+
+          *start++ = '0';
+          *start++ = 'x';
+          while (*ptr) {
+            *start++ = *ptr++;
+          }
+          break;
+
+        default:
+          printf("unknow case %s\n", fmt);
+          assert(0);
+      }
+    } else {
+      *start = *fmt;
+      start++;
+    }
+    fmt++;
+  }
+  *start = '\0';
+  return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-	va_list ap;
-	int ret;
-	va_start(ap, fmt);
-	ret = vsprintf(out, fmt, ap);
-	va_end(ap);
-
-	return ret;
-}
-
-int printf(const char *fmt, ...) {
-	va_list ap;
-	va_start(ap, fmt);
-	my_format(NULL, _printf_putc, fmt, ap);
-	va_end(ap);
-	return 0;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-
+  va_list ap;
+  va_start(ap, fmt);
+  vsprintf(out, fmt, ap);
+  va_end(ap);
   return 0;
 }
+
+int snprintf(char *out, size_t n, const char *fmt, ...) { return 0; }
+
 
 #endif
