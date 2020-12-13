73681dd1966b7ae6d4e525de4b9a2cd33feb0a3d
tracer-ics2019 <tracer@njuics.org>
2020-12-13 05:43:11 -0800

    >  compile U201713732 jiriyu Linux ubuntu 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:43:11 up 2 days,  9:02,  1 user,  load average: 0.35, 0.24, 0.20 b2e6e73f9417ee756f12581fb559aa37dbafba84

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -9,7 +9,7 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
 	decinfo.isa.sepc = epc;
 	decinfo.isa.scause = NO;
 	decinfo.jmp_pc = decinfo.isa.stvec;
-	decinfo.is_jmp = 1;
+	decinfo_set_jmp(true);
 }
 
 bool isa_query_intr(void) {
