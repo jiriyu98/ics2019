0bc62a42daebbf42aab7f40f700ea7e591e34cec
tracer-ics2019 <tracer@njuics.org>
2020-12-20 02:34:39 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:34:39 up  7:19,  1 user,  load average: 0.19, 0.23, 0.18 a90489344edc0938bb218c69c795dabe348a52d2

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -39,13 +39,11 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  while (*s1 != '\0' && *s1 == *s2)
-    {
-      s1++;
-      s2++;
-    }
-
-  return (*(unsigned char *) s1) - (*(unsigned char *) s2);
+	while(*s1 && *s2 && *s1 == *s2){
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
