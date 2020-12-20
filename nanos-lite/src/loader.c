c9d31031304d6c5958363f10cd343eda565b4ca7
tracer-ics2019 <tracer@njuics.org>
2020-12-20 05:37:42 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:37:42 up 10:22,  1 user,  load average: 0.42, 0.27, 0.22 281908e567e9e2288f1b660e1e2c403fee41625f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -54,7 +54,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  intptr_t entry = loader(pcb, filename);
+  uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = 0x%x", entry);
   ((void(*)())entry) ();
 }
--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -2,7 +2,7 @@
 
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
-#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start) - 8)
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
 
 /* The kernel is monolithic, therefore we do not need to
  * translate the address `buf' from the user process to
@@ -12,20 +12,20 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  memcpy(buf, &ramdisk_start + 8 + offset, len);
+  memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
 size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  memcpy(&ramdisk_start + 8 + offset, buf, len);
+  memcpy(&ramdisk_start + offset, buf, len);
   return len;
 }
 
 void init_ramdisk() {
   Log("ramdisk info: start = %p, end = %p, size = %d bytes",
-      &ramdisk_start + 8, &ramdisk_end, RAMDISK_SIZE);
+      &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
 size_t get_ramdisk_size() {
