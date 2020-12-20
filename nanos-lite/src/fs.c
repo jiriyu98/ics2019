2041220011104166e45eeac6adfdf639ff404f88
tracer-ics2019 <tracer@njuics.org>
2020-12-19 22:39:06 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:39:06 up  3:23,  1 user,  load average: 0.61, 0.30, 0.21 b4058beebf6cbddd4fda679856b7b4f49d5d4304

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,11 +38,11 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/events", 0xffffff, 0, 0, events_read, invalid_write},
-  {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
-  {"/dev/fbsync", 0xffff, 0, 0, invalid_read, fbsync_write},
-  {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
+  // {"/dev/events", 0xffffff, 0, 0, events_read, invalid_write},
+  // {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
+  // {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
+  // {"/dev/fbsync", 0xffff, 0, 0, invalid_read, fbsync_write},
+  // {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
