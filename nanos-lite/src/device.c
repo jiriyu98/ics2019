9c8898f0f276170f17380fec13165bce1e76f2fe
tracer-ics2019 <tracer@njuics.org>
2020-12-19 23:00:07 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:00:07 up  3:44,  1 user,  load average: 0.30, 0.30, 0.27 ac33d61e8c2fed4ff114e2a8b56ae778d6197cb9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,10 +1,18 @@
 #include "common.h"
 #include <amdev.h>
+#include <am.h>
+#include <nemu.h>
+
+extern int screen_width();
+extern int screen_height();
+
+size_t __am_input_read(uintptr_t reg, void *buf, size_t size);
+size_t __am_timer_read(uintptr_t reg, void *buf, size_t size);
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  for(int i = 0; i < len;i++){
-    _putc(((char*)buf)[i]);
-  }
+    for(int i = 0; i < len;i++){
+        _putc(((char*)buf)[i]);
+    }
   return len;
 }
 
@@ -17,20 +25,37 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+    int kc = read_key();
+    char tmp[3]="ku";
+    if((kc & 0xfff) == _KEY_NONE){
+        int time = uptime();
+        len = sprintf(buf,"t %d\n", time);
+    }
+    else{
+        if(kc&0x8000)
+            tmp[1] = 'd';
+        len = sprintf(buf,"%s %s\n",tmp,keyname[kc&0xfff]);
+    }
+  return len;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+    len = sprintf(buf, dispinfo + offset);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+    int x = (offset/4) % screen_width();
+    int y = (offset/4) / screen_width();
+    draw_sync();
+    draw_rect((uint32_t*)buf, x, y, len/4, 1);
+  return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+    draw_sync();
   return 0;
 }
 
@@ -38,6 +63,5 @@ void init_device() {
   Log("Initializing devices...");
   _ioe_init();
 
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
