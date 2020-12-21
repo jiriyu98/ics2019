95d0e14e325f4af2cca0bd27bf678f8f22bc00f8
tracer-ics2019 <tracer@njuics.org>
2020-12-21 06:17:33 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:17:33 up 14:08,  1 user,  load average: 0.27, 0.24, 0.20 6010085aab7ec8edc4d979b55a9ca2e1f30f74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,40 +3,8 @@
 #include "proc.h"
 #include "syscall.h"
 
-extern void naive_uload(PCB *pcb, const char *filename);
-static int programBrk;
+int sys_exit(){
 
-int do_open(const char*path, int flags, int mode){
-    int res = fs_open(path, flags, mode);
-    return res;
-}
-
-int do_close(int fd){
-    int res = fs_close(fd);
-    return res;
-}
-
-int do_read(int fd, void*buf, size_t count){
-    if(fd>=0 && fd<=2){
-        return 0;
-    }
-    int res = fs_read(fd, buf, count);
-    return res;
-}
-
-int do_write(int fd, const void*buf, size_t count){
-    int res = fs_write(fd, buf, count);
-    return res;
-}
-
-size_t do_lseek(int fd, size_t offset, int whence){
-    size_t res = fs_lseek(fd, offset, whence);
-    return res;
-}
-
-int do_brk(int addr){
-    programBrk = addr;
-    return 0;
 }
 
 _Context* do_syscall(_Context *c) {
@@ -47,38 +15,34 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-      case SYS_exit:
-          naive_uload(NULL,"/bin/init");
-          _halt(a[1]);
-          break;
-      // case SYS_yield:
-      //     _yield();
-      //     c->GPRx = 0;
+      // case SYS_exit:
+      //     _halt(0);
       //     break;
-      case SYS_write:
-          c->GPRx = do_write(a[1], (void*)(a[2]), a[3]);
+      case SYS_yield:
+          _yield();
+          c->GPRx = 0;
           break;
-      case SYS_read:
-          c->GPRx = do_read(a[1], (void*)(a[2]), a[3]);
-          break;
-      case SYS_lseek:
-          c->GPRx = do_lseek(a[1], a[2], a[3]);
-          break;
-      case SYS_open:
-          c->GPRx = do_open((const char *)a[1], a[2], a[3]);
-          break;
-      case SYS_close:
-          c->GPRx = do_close(a[1]);
-          break;
-      case SYS_brk:
-          c->GPRx = do_brk(a[1]);
-          break;
-      case SYS_execve:
-          printf("%s\n", a[1]);
-          naive_uload(NULL, (const char*)a[1]);
-          c->GPR2 = SYS_exit;
-          do_syscall(c);
-          break;
+      // case SYS_write:
+      //     c->GPRx = do_write(a[1], (void*)(a[2]), a[3]);
+      //     break;
+      // case SYS_read:
+      //     c->GPRx = do_read(a[1], (void*)(a[2]), a[3]);
+      //     break;
+      // case SYS_lseek:
+      //     c->GPRx = do_lseek(a[1], a[2], a[3]);
+      //     break;
+      // case SYS_open:
+      //     c->GPRx = do_open((const char *)a[1], a[2], a[3]);
+      //     break;
+      // case SYS_close:
+      //     c->GPRx = do_close(a[1]);
+      //     break;
+      // case SYS_brk:
+      //     c->GPRx = do_brk(a[1]);
+      //     break;
+      // case SYS_execve:
+      //     c->GPRx = sys_execve((void *)a[1], (char *const *)(void *)a[2], (char *const *)(void *)a[3]);
+      //     break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
