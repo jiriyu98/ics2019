85ff0cf23cd9bf50fadab20f605272607b1d6e1b
tracer-ics2019 <tracer@njuics.org>
2020-12-21 22:24:52 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:24:52 up 16:25,  1 user,  load average: 0.35, 0.27, 0.23 b16982d7185942f457779fde38784df2e06b551

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -12,7 +12,7 @@ int main() {
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
-    printf("test:%d\n", i);
+    printf("test:i:%d, &n=%d\n", i, &n);
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
