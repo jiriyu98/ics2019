46383bda374484b485a2edc6ee9b88eadb9d3edf
tracer-ics2019 <tracer@njuics.org>
2020-12-22 05:35:47 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:35:47 up 20:58,  1 user,  load average: 0.30, 0.24, 0.19 b2fdb8a531b6390bb77e45b0df2b79d7c89b8c14

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,7 +26,6 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
     int keycode = read_key();
-    char tmp[3]="ku";
     if((keycode & ~0x8000) == _KEY_NONE){
         len = sprintf(buf,"t %d\n", uptime());
     }
