77de27a56de7a73341fba31731eead8955442ead
tracer-ics2019 <tracer@njuics.org>
2020-12-22 07:02:13 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:02:13 up 22:24,  1 user,  load average: 0.69, 0.37, 0.30 130896473312183e3a2a614b269e4abf8f7446fa

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -134,6 +134,7 @@ static void get_display_info() {
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
+    printf("w%s\n", buf);
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
@@ -141,8 +142,6 @@ static void get_display_info() {
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
   fclose(dispinfo);
-  printf("%d\n", screen_w);
-  printf("%d\n", screen_h);
   assert(screen_w > 0 && screen_h > 0);
 }
 
