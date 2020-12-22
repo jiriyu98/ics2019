e632125044a70d19fd12713cffab731b30e727c1
tracer-ics2019 <tracer@njuics.org>
2020-12-22 07:56:17 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:56:17 up 23:18,  1 user,  load average: 0.34, 0.28, 0.27 940aefaf1ba35ced9dec3d8b4d0034b79f53c3c2

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,17 +1,21 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
 #include <klib.h>
-static uint32_t *const fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
+
+#define W 400
+#define H 300
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = inl(SCREEN_ADDR);
-      info->height = info->width & 0xffff;
-    info->width = info->width >> 16;
+      info->width = W;
+      info->height = H;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -21,25 +25,33 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
 size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
-      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-    int width = screen_width(), height = screen_height(), i;
-    assert(ctl->x + ctl->w <= width && ctl->y + ctl->h <= height);
-    for (i = 0; i < ctl->h; i++)
-    memcpy(fb + (i + ctl->y) * width + ctl->x, ctl->pixels + i * ctl->w, ctl->w * sizeof(uint32_t));
-
+        _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+        int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+        
+      /* fb: the whole data that will be painted on the screen, 
+       * but we only need to fill part of data (that is from (x, y) to (x + w, y + h)) by copy from pixels
+       */
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; 
+      
+      /* pixels: the data that will be painted. we show copy the data in pixels to fb. */
+      uint32_t* pixels = ctl->pixels;
+      int copy_size = sizeof(uint32_t) * ( W - x < w ? W-x : w);
+      for(int i = 0; i < h && y + i < H; ++ i) {
+        memcpy(&fb[(i + y) * W + x], pixels + i * w, copy_size);
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
       return size;
-    }
+    } 
   }
   return 0;
 }
 
 void __am_vga_init() {
-  
-  int i, size = screen_width() * screen_height();
-  for (i = 0; i < size; i++) fb[i] = i;
+  int i;
+  int size = screen_width() * screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i ++) fb[i] = i;
   draw_sync();
-  
 }
