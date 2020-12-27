151ee66543dd377ff9640687e3e909d463220f6b
tracer-ics2019 <tracer@njuics.org>
2020-12-27 04:18:30 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:18:30 up 3 days,  2:05,  1 user,  load average: 0.78, 0.46, 0.28 aa8d9e134520bc6af97d7f10fe807a26a410ff5a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,16 +21,9 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  // switch_boot_pcb();
-
-  // Log("Initializing processes...");
-  // naive_uload(NULL, "/bin/init");
   // context_kload(&pcb[0], (void *)hello_fun);
-  // switch_boot_pcb();
-  
-  context_kload(&pcb[0], (void *)hello_fun);
   switch_boot_pcb();
-
+  context_uload(&pcb[0], "/bin/hello");
   context_uload(&pcb[1], "/bin/init");
 }
 
