15c0dca90751f43c3051cff15c100ab669eb33ae
tracer-ics2019 <tracer@njuics.org>
2020-12-29 03:59:14 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:59:14 up 3 days, 18:07,  1 user,  load average: 0.26, 0.26, 0.21 4e5f184d8ac57d696ac6a36119df9ae21f94886

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -40,8 +40,8 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
       case SYS_exit:
-          naive_uload(NULL,"/bin/init");
-          // _halt(a[1]);
+          // naive_uload(NULL,"/bin/init");
+          _halt(a[1]);
           break;
       case SYS_yield:
           _yield();
