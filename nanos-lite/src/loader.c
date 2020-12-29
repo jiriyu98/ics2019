f6731f6811afcee607f3ecbfe4c0601b49ded67f
tracer-ics2019 <tracer@njuics.org>
2020-12-29 05:46:30 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:46:30 up 3 days, 19:54,  1 user,  load average: 0.21, 0.20, 0.18 5b296f58d06c54d46bdf7b82e13a1a2b37745655

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,9 +45,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
           count++;
           printf("0x%x  0x%x\n",vaddr,paddr);
           _map(&pcb->as,vaddr,paddr,0);
+          printf("0x%x  0x%x\n",vaddr,paddr);
           fs_read(fd,paddr,read_bytes);
           pcb->max_brk = (uintptr_t)vaddr+PGSIZE;
-          vaddr+=PGSIZE;
+          vaddr += PGSIZE;
         }
         memset((void*)paddr-(count-1)*PGSIZE+Phdr.p_filesz,0,(Phdr.p_memsz-Phdr.p_filesz));
 #else
