127b35de033519094af0eef1592fce649a77d246
tracer-ics2019 <tracer@njuics.org>
2020-12-29 21:19:17 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:19:17 up 3 days, 23:45,  1 user,  load average: 0.26, 0.24, 0.26 2a9791c1eb9f1a3b187fdfada78ef839586754ff

--- nexus-am/am/src/riscv32/nemu/vme.c
+++ nexus-am/am/src/riscv32/nemu/vme.c
@@ -89,12 +89,9 @@ int _map(_AddressSpace *as, void *va, void *pa, int prot) {
     *pptab = (uint32_t)pgalloc_usr(1);
     *pptab = *pptab | PTE_V;
   }
-  printf("pptab : %x\n", pptab);
   PDE *ptab = &(((PDE *)PTE_ADDR(*pptab))[PTX(va)]);
-  printf("PPN = 0x%x\n", PTE_ADDR(pa));
-  printf("%x\n", *ptab);
   if (!(*ptab & PTE_V)) { 
-    *ptab = PTE_ADDR(pa) | PTE_V;
+    *ptab = ((uint32_t)pa >> 12 << 10) | PTE_V;
   }
 
   return 0;
