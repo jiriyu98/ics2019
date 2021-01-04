5197c2c64028ef4309a168f730eb222218ab4a38
tracer-ics2019 <tracer@njuics.org>
2021-01-04 07:58:26 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:58:26 up 1 day, 14:04,  1 user,  load average: 0.33, 0.37, 0.31 a8f7db423bc2512d2f21ab7193ff3277b54f2980

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,8 +19,7 @@ static inline int sys_read(int fd, void*buf, size_t len){
 }
 
 static inline int sys_write(int fd, const void*buf, size_t len){
-    Log("%s", buf);
-    assert(0);
+    Log("2");
     return fs_write(fd, buf, len);
 }
 
