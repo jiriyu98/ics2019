2e8d812f600d789f647c975e01a12d25e732490b
tracer-ics2019 <tracer@njuics.org>
2021-01-05 06:52:39 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:52:39 up 2 days, 17 min,  1 user,  load average: 0.34, 0.28, 0.27 e7b1bc0f9afc783bc85fb1bc84f0f102adbd0b3e

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,8 +26,8 @@ void hello_fun(void *arg) {
 void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun);
   // context_kload(&pcb[0], (void *)hello_fun);
-  context_uload(&pcb[0], "/bin/pal");
-  // context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[0], "/bin/hello");
+  context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
 }
 
@@ -36,8 +36,8 @@ _Context* schedule(_Context *prev) {
   current->cp = prev;
 
   // always select pcb[0] as the new process
-  current = &pcb[0];
-  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  // current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   // then return the new context
   return current->cp;
