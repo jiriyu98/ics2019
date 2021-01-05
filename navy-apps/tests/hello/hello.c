67b63fa8c1624d3679e8de3b839a6845acdf2d32
tracer-ics2019 <tracer@njuics.org>
2021-01-05 07:41:34 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:41:34 up 2 days,  1:06,  1 user,  load average: 0.32, 0.30, 0.31 91e1a10d82a6c6342ce3c90c2975ff5b9ce15b5f

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  write(1, "Hello World!\n", 13);
+  // write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
+    // write(1, "Hello World!\n", 13);
     j ++;
     if (j == 10000) {
+      write(1, "Hello World!\n", 13);
       printf("Hello World from Navy-apps for the %dth time!\n", i++);
       j = 0;
     }
