95d4062e4202e000bb04005a43a6799727dcb357
tracer-ics2019 <tracer@njuics.org>
2021-01-05 07:42:19 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:42:19 up 2 days,  1:07,  1 user,  load average: 1.07, 0.50, 0.38 5891dab3125dc6c2c66f4157f9c3c047cd4a3883

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -8,7 +8,8 @@ int main() {
   while (1) {
     j ++;
     if (j == 10000) {
-      printf("Hello World  %dth time!\n", i++);
+      printf("Hello World from Navy-apps for the %dth time!\n", i);
+      i++;
       j = 0;
     }
   }
