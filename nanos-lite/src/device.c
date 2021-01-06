f6af27f680d2636d3e73f530084f3d5306bdad85
tracer-ics2019 <tracer@njuics.org>
2021-01-06 05:58:59 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:58:59 up 2 days,  9:43,  1 user,  load average: 0.35, 0.34, 0.28 1b2351df272e748dee788dc8d2cb838439d63a03

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -51,6 +51,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  _yield();
+
     int x = (offset/4) % screen_width();
     int y = (offset/4) / screen_width();
     // draw_sync();
