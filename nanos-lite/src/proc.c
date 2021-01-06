1fccded74aa41f48ada0ae8f77b024469b2f8117
tracer-ics2019 <tracer@njuics.org>
2021-01-05 22:25:02 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:25:02 up 2 days,  5:30,  1 user,  load average: 0.95, 0.39, 0.23 92782bc61b7c2a6c38d95dab63cf1bce98484bb3

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun);
   // context_kload(&pcb[0], (void *)hello_fun);
   // context_uload(&pcb[0], "/bin/dummy");
-  context_uload(&pcb[0], "/bin/hello");
+  context_uload(&pcb[0], "/bin/pal");
   switch_boot_pcb();
 }
 
