3a54c2c3a550cbd98eb56157ed668ac4caae969e
tracer-ics2019 <tracer@njuics.org>
2021-01-06 06:07:18 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:07:18 up 2 days,  9:51,  1 user,  load average: 0.55, 0.46, 0.34 72979f2e4f977bf31ccb9d21f3cfb7182b46c83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -62,7 +62,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
     draw_sync();
-  return len;
+  return 0;
 }
 
 void init_device() {
