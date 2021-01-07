b4394b6a96f17eeee2ea55bd9d10e4f3612e8d63
tracer-ics2019 <tracer@njuics.org>
2021-01-07 04:19:52 -0800

    >  run U201713732 jiriyu Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:19:52 up 2 days, 16:26,  1 user,  load average: 0.66, 0.33, 0.23 2d140b2b81c5920dfe40f30895bc97dca6cccc6a

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -14,7 +14,7 @@ _Context* __am_irq_handle(_Context *c) {
       case 0x80000005:
           ev.event = _EVENT_IRQ_TIMER;
           break;
-      case 0x80000001:
+      case -1:
           ev.event = _EVENT_YIELD;
           break;
       case 0:
